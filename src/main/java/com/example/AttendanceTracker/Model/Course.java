406a7caaec24ef8b568a051ebf19ad897bbc75a9
Devaram S <devarams569@gmail.com>
2025-07-18 06:33:40 +0530

    added teacher entity to retrieve teacher courses

--- src/main/java/com/example/AttendanceTracker/Model/Course.java
+++ src/main/java/com/example/AttendanceTracker/Model/Course.java
@@ -12,15 +12,38 @@ import java.util.List;
 @Data
 
 @NoArgsConstructor
-
+@AllArgsConstructor
 public class Course {
+    public int getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(int courseID) {
+        this.courseID = courseID;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseSemester() {
+        return courseSemester;
+    }
+
+    public void setCourseSemester(String courseSemester) {
+        this.courseSemester = courseSemester;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int courseID;
 
     private String courseName;
     private String courseSemester;
-
 
     // constructors, getters, setters
 }
